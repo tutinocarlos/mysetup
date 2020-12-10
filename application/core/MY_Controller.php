d990858e7ba7262b8ee450a43f2d9c7de2f0b941
tutinocarlos <tutinocarlos@gmail.com>
2020-12-10 15:59:07 -0300

    views

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -63,12 +63,12 @@ class MY_controller extends CI_Controller {
 
     if($this->email->send())
     {
-        return TRUE;
+//        return TRUE;
     }else{
- 		echo 	$this->email->print_debugger();
+ 		 print_r($this->email->print_debugger(), true);
 		}
-			die();
-    return FALSE;
+	
+//    return FALSE;
 
 		}	
 }
