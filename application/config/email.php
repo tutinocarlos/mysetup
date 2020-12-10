72b3b7433e455efcb2f5a81afeb65d244cb34e4a
tutinocarlos <tutinocarlos@gmail.com>
2020-12-10 15:44:32 -0300

    views

--- application/config/email.php
+++ application/config/email.php
@@ -2,7 +2,7 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $config['protocol'] = 'smtp';
 // $config['mail_path'] = 'mail.mysetup.com.ar';
- $config['smtp_host'] = 'mail.mysetup.com.ar';
+ $config['smtp_host'] = 'localhost';
 // $config['smtp_port'] = 465;
  $config['smtp_port'] = 587;
  $config['smtp_user'] = 'no-reply@mysetup.com.ar';
@@ -10,5 +10,5 @@ $config['protocol'] = 'smtp';
  $config['charset'] = "utf-8";
  $config['mailtype'] = "html";
 	$config['smtp_crypto'] = 'tls';
-//	$config['smtp_crypto'] = 'ssl';
+	$config['smtp_crypto'] = 'ssl';
  $config['newline'] = "\r\n";
